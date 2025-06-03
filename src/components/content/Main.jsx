d34821fc3aa9be133bb551581d8357767265731d
Liliia Galkina <vvlili@yandex.ru>
2025-06-03 16:47:28 +0300

    исправлены ошибки реализации перемещения

--- src/components/content/Main.jsx
+++ src/components/content/Main.jsx
@@ -5,24 +5,21 @@ import CardSelect from "./CardSelect";
 import { data } from "../../tasks";
 
 function Main() {
-    const initialTasks = () => {
-        const tasksStr = localStorage.getItem("tasks");
-        return tasksStr ? JSON.parse(tasksStr) : data;
-      };
-    const [tasksAll, setTasksAll] = useState(initialTasks);
+  const initialTasks = () => {
+    const tasksStr = localStorage.getItem("tasks");
+    return tasksStr ? JSON.parse(tasksStr) : data;
+  };
 
-      useEffect(() => {
-        localStorage.setItem("tasks", JSON.stringify(tasksAll));
-      }, [tasksAll]);  
+  const [tasksAll, setTasksAll] = useState(initialTasks);
 
-
-
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasksAll));
+  }, [tasksAll]);
 
   const tasks1 = tasksAll.filter((elem) => elem.isBacklog === true);
   const tasks2 = tasksAll.filter((elem) => elem.isReady === true);
   const tasks3 = tasksAll.filter((elem) => elem.isProgress === true);
   const tasks4 = tasksAll.filter((elem) => elem.isFinished === true);
-
 
   function makeId() {
     const strsource =
@@ -54,9 +51,9 @@ function Main() {
       newTask.name = inputTask.value;
       newTask.description = "This task has no description";
       newTask.isBacklog = true;
-      newTask.isReady= false;
-      newTask.isProgress= false;
-      newTask.isFinished= false;
+      newTask.isReady = false;
+      newTask.isProgress = false;
+      newTask.isFinished = false;
       setTasksAll([...tasksAll, newTask]);
       inputTask.value = "";
     }
@@ -65,7 +62,7 @@ function Main() {
     buttonAdd.style.display = "flex";
   }
 
-  function moveItem(e){
+  function moveItem(e) {
     const btnAddTask = e.target.parentElement;
     const selectTasks = btnAddTask.previousElementSibling;
     const btnSelectSubmit = btnAddTask.nextElementSibling;
@@ -74,27 +71,30 @@ function Main() {
     btnSelectSubmit.style.display = "block";
   }
 
-
-
-  function changeOption(e){
-   let id = e.target.previousElementSibling.previousElementSibling.value
-   console.log(id)
-   setTasksAll(tasksAll.map(task => {
-    if(task.id === id){
-        if(task.isProgress === true){
+  function changeOption(e) {
+    let id = e.target.previousElementSibling.previousElementSibling.value;
+    setTasksAll(
+      tasksAll.map((task) => {
+        if (task.id === id) {
+          if (task.isProgress === true) {
             task.isProgress = false;
             task.isFinished = true;
-        }
-        if(task.isReady === true){
+          }
+          if (task.isReady === true) {
             task.isReady = false;
             task.isProgress = true;
-        }
-        if(task.isBacklog === true){
+          }
+          if (task.isBacklog === true) {
             task.isBacklog = false;
             task.isReady = true;
-        }     
-    }
-   }))
+          }
+        }
+        return task;
+      })
+    );
+    e.target.style.display = "none";
+    e.target.previousElementSibling.style.display = "flex";
+    e.target.previousElementSibling.previousElementSibling.style.display = "none";
   }
 
   return (
@@ -104,9 +104,28 @@ function Main() {
         inputNewTask={inputNewTask}
         submitNewTask={submitNewTask}
       />
-      <CardSelect title="Ready" items={tasks2} newItems={tasks1} selectType="ready" moveItem={moveItem} changeOption={changeOption}/>
-      <CardSelect title="In Progress" items={tasks3} newItems={tasks2} selectType="progress" moveItem={moveItem}/>
-      <CardSelect title="Finished" items={tasks4} newItems={tasks3} selectType="finish" moveItem={moveItem}/>
+      <CardSelect
+        title="Ready"
+        items={tasks2}
+        newItems={tasks1}
+        selectType="ready"
+        moveItem={moveItem}
+        changeOption={changeOption}
+      />
+      <CardSelect
+        title="In Progress"
+        items={tasks3}
+        newItems={tasks2}
+        selectType="progress"
+        moveItem={moveItem}
+      />
+      <CardSelect
+        title="Finished"
+        items={tasks4}
+        newItems={tasks3}
+        selectType="finish"
+        moveItem={moveItem}
+      />
     </main>
   );
 }
