ab33793c6cae270f8c20b917a9cc25d20b6610f6
Liliia Galkina <vvlili@yandex.ru>
2025-06-05 16:38:33 +0300

    исправлено меню

--- src/components/header/Menu.jsx
+++ src/components/header/Menu.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { useState } from "react";
 import Logo from "../../img/user-avatar.svg"
 import arrow from "../../img/arrow-down-white.svg"
 import style from "./header.module.scss"
 
 export default function Menu(){
+    const [isMenuVisible, setIsMenuVisible] = useState(false);
 
-        function showMenu(){
+       /* function showMenu(){
             const menu = document.querySelector("#menu");
             let logoArrow = document.querySelector("#logoArrow");
           
@@ -16,19 +18,39 @@ export default function Menu(){
             menu.style.display = "none";
             logoArrow.style.transform = "rotate(0deg)"
            }
-        }
+        }*/
+
+        const toggleMenu = () => {
+            setIsMenuVisible(!isMenuVisible);
+        };
   
-    
     return (
-        <nav className={style.menu} onClick={showMenu}>
-            <div className={style.logo}>
-                <div className={style.logo__user}><img src={Logo} alt="логотип пользователя" /></div>
-                <div className={style.logo__arrow}><img src={arrow} id="logoArrow" alt="белая стрелочка вниз/вверх" /></div>
+        <nav className={style.menu} onClick={toggleMenu}>
+        <div className={style.logo}>
+            <div className={style.logo__user}>
+                <img src={Logo} alt="логотип пользователя" />
             </div>
-            <ul id="menu" className={style.menu__list} >
-                <li className={style.menu__item}><a href="#" className={style.menu__link}>Profile</a></li>
-                <li className={style.menu__item}><a href="#" className={style.menu__link}>Log Out</a></li>
-            </ul>
-        </nav>
+            <div className={style.logo__arrow}>
+                <img
+                    src={arrow}
+                    id="logoArrow"
+                    alt="белая стрелочка вниз/вверх"
+                    style={{ transform: isMenuVisible ? 'rotate(180deg)' : 'rotate(0deg)' }}
+                />
+            </div>
+        </div>
+        <ul
+            id="menu"
+            className={style.menu__list}
+            style={{ display: isMenuVisible ? 'block' : 'none' }}
+        >
+            <li className={style.menu__item}>
+                <a href="#" className={style.menu__link}>Profile</a>
+            </li>
+            <li className={style.menu__item}>
+                <a href="#" className={style.menu__link}>Log Out</a>
+            </li>
+        </ul>
+    </nav>
     )
 }
